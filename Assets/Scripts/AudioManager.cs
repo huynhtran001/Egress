f4e326fcaf446282d1188c3bbcaa52d3e26f2933
Huynh <huynhtran117@yahoo.com>
2020-05-08 18:41:09 -0400

    Added audio manager script for music

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip[] clips;
+
+    private void Awake()
+    {
+        // Singleton
+        int x = FindObjectsOfType<AudioManager>().Length;
+
+        if (x > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        PlayClip(clips[currentScene]);
+    }
+
+    // Plays the audioclip passed into this function
+    void PlayClip(AudioClip clip)
+    {
+        audioSource.Stop();
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+}
